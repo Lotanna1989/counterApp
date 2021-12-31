5e99a53a86488090966ec01e465e39c49b20c52d
Lotanna1989 <78662437+Lotanna1989@users.noreply.github.com>
2021-12-31 18:14:15 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let count = 0
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+function decrement() {
+    count -= 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count - " - "
+    saveEl.textContenttStr -= countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+console.log("Let's count people on the subway!")
+
+console.log("Let's count people on the subway!")
